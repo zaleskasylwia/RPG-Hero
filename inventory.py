4f1950f11c98a791dde9f9848385f98d4b2fbfa8
sylwia <zaleska.syl@gmail.com>
2017-08-06 09:54:46 +0200

    Add method to add item to inventory

--- inventory.py
+++ inventory.py
@@ -4,4 +4,10 @@ class Inventory:
         self.max_weight = max_weight
         self.items = items or []
 
+    def add_item(self, item):
+        if item < self.capacity or item < self.max_weight:
+            return self.items.append(item)
+        else:
+            #raise?
+            print("It's too heavy")
 
