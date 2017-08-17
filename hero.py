f7c21faf855ffa0c29c17786e860dad50a5aefcb
sylwia <zaleska.syl@gmail.com>
2017-08-17 17:36:31 +0200

    Add method get items and pick an item

--- hero.py
+++ hero.py
@@ -4,16 +4,23 @@ from inventory import Inventory
 
 class Hero:
     def __init__(self, first_name, last_name, race):
-        self.character = Character()
-        self.inventory = Inventory()
-        self.experience = experience
-        self.level = level
+        if type(first_name) is str nad type(last_name) is str and type(race) is str:
+            self.character = Character(first_name, last_name, race)
+            self.inventory = Inventory()
+            self.experience = experience
+            self.level = level
 
     def get_items(self):
-        pass
+        items = [item for item in self.inventory]
+        return items
 
     def pick_an_item(self, item):
-        pass
+        items = []
+        if item not in self.inventory:
+            items.append(item)
+        return items
 
     def drop_item(self, item):
+        if item in self.inventory:
+
         pass
